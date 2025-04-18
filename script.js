2493b68b6f662263abc034f99f367bdea434d666
Ducksonkeyboard <196486687+Ducksonkeyboard@users.noreply.github.com>
2025-04-18 10:15:25 -0700

    Create getComputerChoice
    This function will randomly generate three
    outputs, which will map to Rock,Paper and
    scissors.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("hello world")
+function getComputerChoice (){
+    // Math.random will generate a floating number (0-1) and 
+    // then be multiped by 3 and then rounded down
+    // Can return either 0, 1, or 2. 
+    return Math.floor(Math.random() * 3);
+}
+console.log(getComputerChoice())
