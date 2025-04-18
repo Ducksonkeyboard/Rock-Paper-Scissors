8703a53acea7e3ed78a11cb59b7a176b587a4ac5
Ducksonkeyboard <196486687+Ducksonkeyboard@users.noreply.github.com>
2025-04-18 10:55:50 -0700

    Create function to get user input
    getHumanChoice will prompt the user to input
    either rock, paper, or scissors.

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 console.log("hello world")
+//will return either rock, paper, or scissors.
 function getComputerChoice (){
-    // Math.random will generate a floating number (0-1) and 
-    // then be multiped by 3 and then rounded down
-    // Can return either 0, 1, or 2. 
-    let num = Math.floor(Math.random() * 3);
-    /*
-    When number is 0 then
-        rock
-    When number is 1 then 
-        paper
-    when number is 2 then
-        scissors
-    end
+    /* 
+    Math.random will generate a floating number (0-1) and 
+    then be multiped by 3 and then rounded down
+    Can return either 0, 1, or 2. 
     */
+    let num = Math.floor(Math.random() * 3);
+
     if (num == 0){
         return "rock"
     } else if(num == 1){
@@ -21,4 +16,10 @@ function getComputerChoice (){
         return "scissors"
     }
 }
+
+function getHumanChoice(){
+    return prompt("Pick either rock, paper or scissors.")
+}
+
 console.log(getComputerChoice())
+console.log("User picked "+ getHumanChoice())
