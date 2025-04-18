e32011e9635a8b9f8b4da71101db136f348829a1
Ducksonkeyboard <196486687+Ducksonkeyboard@users.noreply.github.com>
2025-04-18 10:36:59 -0700

    Map getComputerChoice to Rock Paper Scissors

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ function getComputerChoice (){
     // Math.random will generate a floating number (0-1) and 
     // then be multiped by 3 and then rounded down
     // Can return either 0, 1, or 2. 
-    return Math.floor(Math.random() * 3);
+    let num = Math.floor(Math.random() * 3);
+    /*
+    When number is 0 then
+        rock
+    When number is 1 then 
+        paper
+    when number is 2 then
+        scissors
+    end
+    */
+    if (num == 0){
+        return "rock"
+    } else if(num == 1){
+        return "paper"
+    } else {
+        return "scissors"
+    }
 }
 console.log(getComputerChoice())
